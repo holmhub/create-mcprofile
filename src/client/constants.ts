34d0acb578c09845598a5ed5368ec5b6dbb29438
holmityd <nbayramov100@gmail.com>
2025-04-16 21:22:15 +0400

    feat(launch): add DEFAULT_URLS constant and forge wrapper args
    Introduce DEFAULT_URLS constant to centralize URLs used in the launcher options. Additionally, add forgeWrapperArgs to handle Forge-specific configurations during launch initialization.

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -1,3 +1,11 @@
 import { EventEmitter } from 'node:events';
 
 export const client = new EventEmitter();
+
+export const DEFAULT_URLS = {
+	meta: 'https://launchermeta.mojang.com',
+	resource: 'https://resources.download.minecraft.net',
+	mavenForge: 'https://files.minecraftforge.net/maven/',
+	defaultRepoForge: 'https://libraries.minecraft.net/',
+	fallbackMaven: 'https://search.maven.org/remotecontent?filepath=',
+} as const;
